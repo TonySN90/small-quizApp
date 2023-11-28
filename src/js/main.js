5d516f8e48a2a7d3a5b2e2dfce4183b113570bf3
TonySN90 <tony.heider90@gmail.com>
2023-11-28 10:21:06 +0100

    update datalist

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,7 @@
 "use strict";
 
 import "./../scss/style.scss";
-// import "data.js";
-
-const questionData = [
-  {
-    category: "Allgemeine Wissenschaft",
-    id: "7392058164",
-    correctAnswer: "Sauerstoff",
-    incorrectAnswers: ["Eisen", "Gold", "Stickstoff"],
-    question: 'Welches Element hat die chemische Abkürzung "O"?',
-  },
-
-  {
-    category: "Geographie",
-    id: "4827365910",
-    correctAnswer: "Canbarra",
-    incorrectAnswers: ["Sydney", "Melbourne", "Brisbane"],
-    question: "Was ist die Hauptstadt von Australien?",
-  },
-
-  {
-    category: "Film und Fernsehen",
-    id: "9051732846",
-    correctAnswer: "Emma Stone",
-    incorrectAnswers: ["Emma Watson", "Natalie Portman", "Meryl Streep"],
-    question:
-      'Wer hat den Oscar für die beste weibliche Hauptrolle im Film "La La Land" gewonnen?',
-  },
-  {
-    category: "Geschichte",
-    id: "6278943105",
-    correctAnswer: "1918",
-    incorrectAnswers: ["1916", "1920", "1939"],
-    question: "In welchem Jahr endete der erste Weltkrieg?",
-  },
-
-  {
-    category: "Musik",
-    id: "1492086375",
-    correctAnswer: "Flöte",
-    incorrectAnswers: ["Geige", "Klavier", "Trompete"],
-    question:
-      'Welches Instrument spielt der Protagonist in Mozart\'s "Die Zauberflöte"?',
-  },
-];
+import { questionData } from "./data.js";
 
 const quizApp = function () {
   const questionEl = document.querySelector("#question");
@@ -155,6 +112,7 @@ const quizApp = function () {
     );
 
     correctBtn.classList.add("correct");
+    state.checked = false;
   };
 
   const initialize = function () {
